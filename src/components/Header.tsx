1bc0aa327e02fa587dc5960e244da5f3989622af
sujinyang00 <sujinyang00@gmail.com>
2023-07-03 20:49:39 +0900

    Style: Header style 수정 완

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,9 @@ const Header = ()=>
 {
 
     return (
-        <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', borderBottom: '1px solid #D9D9D9' }}>
+        <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', }}>
             <img src={IMAGES.logo} alt="Logo" style={{ marginRight: 'auto', marginLeft: '5%' }} />
             <img src={IMAGES.topMenuBar} alt="menu" style={{ marginLeft: 'auto', marginRight: '5%' }} />
-
         </div>
     );
 
