b7519974595044b6349118c7e4cf65c8a118dac0
sujinyang00 <sujinyang00@gmail.com>
2023-07-17 21:38:10 +0900

    Fix: url 약간 수정

--- src/screens/KakaoLogin/KakaoLogin.tsx
+++ src/screens/KakaoLogin/KakaoLogin.tsx
@@ -3,13 +3,15 @@ import {TEXT} from "../../constants/text";
 import React, {useEffect} from "react";
 import axios from "axios";
 
+const defaultUrl = 'https://www.match-api-server.com';
+
 
 const KakaoLogin = () => {
-    const Rest_api_key='8dfa7db4e6e6e29a50acefe5f2016a73' //REST API KEY
-    const redirect_uri = 'https://localhost:3000/auth/kakao' //Redirect URI
+    const REST_API_KEY='8dfa7db4e6e6e29a50acefe5f2016a73' //REST API KEY
+    const REDIRECT_URI = 'https://www.match-api-server.com/auth/kakao' //Redirect URI
 
     // oauth 요청 URL
-    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
+    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
 
     const handleLogin = ()=>{
         window.location.href = kakaoURL
@@ -17,36 +19,6 @@ const KakaoLogin = () => {
 
         const code= new URL(document.location.toString()).searchParams.get('code');
         const grantType = "authorization_code";
-        const REST_API_KEY = Rest_api_key;
-        const REDIRECT_URI = redirect_uri;
-
-
-        /*axios.get(
-            `https://kapi.kakao.com/v2/user/me`, {
-                headers: {
-                    Authorization: `Bearer ${Rest_api_key}`,
-                }
-            })
-            .then((res: any) => {
-                const { access_token } = res.data;
-                console.log('access token : ' + res.data);
-
-                axios.post(
-                    `https://kapi.kakao.com/v2/user/me`,
-                    {},
-                    {
-                        headers: {
-                            Authorization: `Bearer ${access_token}`,
-                        }
-                    }
-                )
-                    .then((res: any) => {
-                        console.log('2번째', res);
-                    })
-            })
-            .catch((Error: any) => {
-                console.log(Error)
-            })*/
 
 
         axios.post(
@@ -71,11 +43,29 @@ const KakaoLogin = () => {
                     .then((res: any) => {
                         console.log('2번째', res);
                     })
+
+                try {
+                    //응답 성공
+                    const response = axios.post(defaultUrl+'/auth/kakao',{
+                        accessToken: `${access_token}`,
+                    });
+                    console.log(response);
+                } catch (error) {
+                    //응답 실패
+                    console.error(error);
+                }
+
+
             })
             .catch((Error: any) => {
                 console.log(Error)
             })
+
+
+
     }
+
+
 
     return (
         <>
