9c90162db75d96f119a69498c201d6605f94d8b4
sujinyang00 <sujinyang00@gmail.com>
2023-07-17 21:26:28 +0900

    Feat: https 설정

--- src/screens/KakaoLogin/KakaoLogin.tsx
+++ src/screens/KakaoLogin/KakaoLogin.tsx
@@ -1,17 +1,80 @@
 import {IMAGES} from "../../constants/images";
 import {TEXT} from "../../constants/text";
-import React from "react";
+import React, {useEffect} from "react";
+import axios from "axios";
 
 
 const KakaoLogin = () => {
     const Rest_api_key='8dfa7db4e6e6e29a50acefe5f2016a73' //REST API KEY
-    const redirect_uri = 'https://www.match-api-server/auth/kakao' //Redirect URI
+    const redirect_uri = 'https://localhost:3000/auth/kakao' //Redirect URI
 
     // oauth 요청 URL
     const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
 
     const handleLogin = ()=>{
-        window.location.href = kakaoURL //Redirect를 KakaoRedirectHandler로
+        window.location.href = kakaoURL
+
+
+        const code= new URL(document.location.toString()).searchParams.get('code');
+        const grantType = "authorization_code";
+        const REST_API_KEY = Rest_api_key;
+        const REDIRECT_URI = redirect_uri;
+
+
+        /*axios.get(
+            `https://kapi.kakao.com/v2/user/me`, {
+                headers: {
+                    Authorization: `Bearer ${Rest_api_key}`,
+                }
+            })
+            .then((res: any) => {
+                const { access_token } = res.data;
+                console.log('access token : ' + res.data);
+
+                axios.post(
+                    `https://kapi.kakao.com/v2/user/me`,
+                    {},
+                    {
+                        headers: {
+                            Authorization: `Bearer ${access_token}`,
+                        }
+                    }
+                )
+                    .then((res: any) => {
+                        console.log('2번째', res);
+                    })
+            })
+            .catch((Error: any) => {
+                console.log(Error)
+            })*/
+
+
+        axios.post(
+            `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${code}`,
+            {},
+            { headers: { "Content-type": "application/x-www-form-urlencoded;charset=utf-8" } }
+        )
+            .then((res: any) => {
+                const { access_token } = res.data;
+                console.log('access token : '+res.data);
+
+                axios.post(
+                    `https://kapi.kakao.com/v2/user/me`,
+                    {},
+                    {
+                        headers: {
+                            Authorization: `Bearer ${access_token}`,
+                            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+                        }
+                    }
+                )
+                    .then((res: any) => {
+                        console.log('2번째', res);
+                    })
+            })
+            .catch((Error: any) => {
+                console.log(Error)
+            })
     }
 
     return (
