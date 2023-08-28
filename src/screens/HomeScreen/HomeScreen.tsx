7d8a3371ff6e7d707713f1a9358950c8493a8825
sujinyang00 <sujinyang00@gmail.com>
2023-08-28 17:43:23 +0900

    Fix: 둘러보기 페이지 까지 전환 완료

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,12 +1,23 @@
 import {IMAGES} from "../../constants/images";
 import React, {Fragment, useEffect} from "react";
 //import {BrowserRouter as Router, Outlet, Route, Routes} from 'react-router-dom';
+import './styles.css';
+
 import axios from "axios";
 import * as process from "process";
 
 const baseUrl = 'https://www.match-api-server.com';
 
 const HomeScreen = ()=> {
+    const REACT_APP_PUBLIC_URL = process.env.REACT_APP_PUBLIC_URL;
+
+    const lookAroundScreenUrl = REACT_APP_PUBLIC_URL+'/lookArround';
+    const handleLookBtn = () => {
+        window.location.href = lookAroundScreenUrl;
+    }
+
+
+
     return (
         <>
             <div>
@@ -19,13 +30,15 @@ const HomeScreen = ()=> {
                 <div>
                     <img src={IMAGES.mainPage3}/>
                 </div>
-                <div>
+                <div className={"main4"}>
                     <img src={IMAGES.mainPage4}/>
                 </div>
 
-                <button >
-
-                </button>
+                <div className={"lookarround_nextpage"}>
+                    <button className={"lookarround-btn"}
+                            onClick={() => handleLookBtn()}
+                    >성냥기부 하러가기</button>
+                </div>
             </div>
         </>
     );
