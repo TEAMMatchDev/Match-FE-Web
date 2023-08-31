acbaea3b53b3239a14554219789499f3b3760932
sujinyang00 <sujinyang00@gmail.com>
2023-08-31 15:21:21 +0900

    #Fix: 결제 금액, 결제일 전달

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 
 import LoginScreen from "./screens/LoginScreen/LoginScreen";
 import KakaoLoginRedirectScreen from "./screens/KakaoLogin/KakaoLoginRedirectScreen";
@@ -26,28 +26,29 @@ function AppRouter() {
         <>
             <BrowserRouter basename={"/"}>
                 <Routes>
-                    <Route path="/" element={<HomeScreen/>} />
-                    <Route path="signUp" element={<SignUpScreen/>} />
-                    <Route path="signIn" element={<LoginScreen/>} />
+                    <Route path="/" element={<HomeScreen/>}/>
+                    <Route path="signUp" element={<SignUpScreen/>}/>
+                    <Route path="signIn" element={<LoginScreen/>}/>
 
-                    <Route path="auth/kakao" element={<KakaoLoginRedirectScreen/>} />
-                    <Route path="auth/naver" element={<NaverLoginRedirectScreen/>} />
+                    <Route path="auth/kakao" element={<KakaoLoginRedirectScreen/>}/>
+                    <Route path="auth/naver" element={<NaverLoginRedirectScreen/>}/>
+
+                    <Route path="lookAround" element={<LookAroundScreen/>}/>
+                    <Route path="detail/:projectId" element={<ProjectDetailScreen/>}/>
+
+                    <Route path="auth/pay" element={<PaymentScreen/>}/>
+                    <Route path="auth/pay1" element={<PaymentScreen1/>}/>
+                    <Route path="auth/pay3" element={<PaymentScreen3/>}/>
+
+                    <Route path="auth/pay/regular" element={<RegularPaymentScreen/>}/>
+                    <Route path="auth/pay/regular/:amount" element={<PaymentScreen3/>}/>
+                    <Route path="auth/pay/onetime" element={<OneTimePaymentScreen/>}/>
 
 
-                    <Route path="auth/pay" element={<PaymentScreen />} />
-                    <Route path="auth/pay1" element={<PaymentScreen1 />} />
-                    <Route path="auth/pay3" element={<PaymentScreen3 />} />
+                    <Route path="auth/pay/redirect" element={<PaymentRedirectScreen/>}/>
+                    <Route path="auth/pay/success" element={<PaymentSuccessRedirectScreen/>}/>
+                    <Route path="auth/pay/fail" element={<PaymentFailRedirectScreen/>}/>
 
-                    <Route path="auth/pay/regular" element={<RegularPaymentScreen />} />
-                    <Route path="auth/pay/onetime" element={<OneTimePaymentScreen />} />
-
-
-                    <Route path="auth/pay/redirect" element={<PaymentRedirectScreen />} />
-                    <Route path="auth/pay/success" element={<PaymentSuccessRedirectScreen />} />
-                    <Route path="auth/pay/fail" element={<PaymentFailRedirectScreen />} />
-
-                    <Route path="lookAround" element={<LookAroundScreen/>} />
-                    <Route path="detail/:projectId" element={<ProjectDetailScreen />} />
 
                 </Routes>
             </BrowserRouter>
