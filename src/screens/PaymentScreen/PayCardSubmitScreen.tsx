8924f3921311de63870cf5210e5a1470b909d72d
sujinyang00 <sujinyang00@gmail.com>
2023-09-05 15:46:24 +0900

    dev brandh

--- src/screens/PaymentScreen/PayCardSubmitScreen.tsx
+++ src/screens/PaymentScreen/PayCardSubmitScreen.tsx
@@ -7,7 +7,11 @@ const PayCardSubmitScreen = () => {
     return (
         <Fragment>
             <div className={"text-container"}>
-                <text className={"sponsored_amount"}>결제 카드 등록</text>
+                <text className={"sponsored_amount"}>결제 카드 등록!!</text>
+
+
+
+
             </div>
         </Fragment>
     )
