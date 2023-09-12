129bad99e2e4c67e983b1a9de4285cd2f73b264a
sujinyang00 <sujinyang00@gmail.com>
2023-09-12 17:42:43 +0900

    fix log

--- src/screens/prologue/PreDonationAccountScreen.tsx
+++ src/screens/prologue/PreDonationAccountScreen.tsx
@@ -65,7 +65,7 @@ const PreDonationAccountScreen = () => {
     const handleCopyClipBoard = async (text: string) => {
         try {
             await navigator.clipboard.writeText(text);
-            alert('클립보드에 링크가 복사되었습니다.');
+            alert('클립보드에 계좌번호가 복사되었습니다.');
         } catch (e) {
             alert('복사에 실패하였습니다');
         }
