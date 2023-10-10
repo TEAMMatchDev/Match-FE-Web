500a0e91f5ac769f98c1a7a77718306bdab7c31d
sujinyang00 <sujinyang00@gmail.com>
2023-10-10 15:39:38 +0900

    결제창 호출 hard

--- src/screens/Payments/PaymentScreen.tsx
+++ src/screens/Payments/PaymentScreen.tsx
@@ -42,26 +42,45 @@ const PaymentScreen: React.FC = () => {
     const impPay = () => {
         if (!window.IMP) return;
         /* 1. 가맹점 식별하기 */
-        const { IMP } = window;
-        IMP.init(storeId); // 가맹점 식별코드
+        //const { IMP } = window;
+        var IMP = window.IMP;
+        IMP.init("imp02276456"); // 가맹점 식별코드
+        requestPay()
 
-        /* 2. 결제 데이터 정의하기 */
-        const data: RequestPayParams = {
-            pg: "nice_v2.iamport00m", // PG사 : https://developers.portone.io/docs/ko/tip/pg-2 참고
-            pay_method: "card", // 결제수단
-            merchant_uid: orderId, // 주문번호
-            amount: amount, // 결제금액
-            name: goodsName, // 주문명
+        function requestPay() {
+            IMP.request_pay(
+                {
+                    pg: "nice_v2.iamport00m", // PG사 : https://developers.portone.io/docs/ko/tip/pg-2 참고
+                    pay_method: "card", // 결제수단
+                    merchant_uid: orderId, // 주문번호
+                    amount: amount, // 결제금액
+                    name: goodsName, // 주문명
 
-            buyer_name: "홍길동", // 구매자 이름
-            buyer_tel: "01012341234", // 구매자 전화번호
-            buyer_email: "example@example.com", // 구매자 이메일
-            buyer_addr: "신사동 661-16", // 구매자 주소
-            buyer_postcode: "06018", // 구매자 우편번호
-        };
-
-        /* 4. 결제 창 호출하기 */
-        IMP.request_pay(data, callback);
+                    buyer_name: "홍길동", // 구매자 이름
+                    buyer_tel: "01012341234", // 구매자 전화번호
+                    buyer_email: "example@example.com", // 구매자 이메일
+                    buyer_addr: "신사동 661-16", // 구매자 주소
+                    buyer_postcode: "06018", // 구매자 우편번호
+                }
+            );
+        }
+        // /* 2. 결제 데이터 정의하기 */
+        // const data: RequestPayParams = {
+        //     pg: "nice_v2.iamport00m", // PG사 : https://developers.portone.io/docs/ko/tip/pg-2 참고
+        //     pay_method: "card", // 결제수단
+        //     merchant_uid: orderId, // 주문번호
+        //     amount: amount, // 결제금액
+        //     name: goodsName, // 주문명
+        //
+        //     buyer_name: "홍길동", // 구매자 이름
+        //     buyer_tel: "01012341234", // 구매자 전화번호
+        //     buyer_email: "example@example.com", // 구매자 이메일
+        //     buyer_addr: "신사동 661-16", // 구매자 주소
+        //     buyer_postcode: "06018", // 구매자 우편번호
+        // };
+        //
+        // /* 4. 결제 창 호출하기 */
+        // IMP.request_pay(data, callback);
     }
     /* 3. 콜백 함수 정의하기 */
     function callback(response: RequestPayResponse) {
---
+++ src/state/userState.ts
@@ -0,0 +1,37 @@
+import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+// 페이지가 변경되더라도 상태관리를 유지하기 위해 사용
+const { persistAtom } = recoilPersist();
+
+export const userNameState = atom({
+    key: 'userNameState',
+    default: '',
+    effects_UNSTABLE: [persistAtom]
+});
+
+export const userTelState = atom({
+    key: 'userTelState',
+    default: '',
+    effects_UNSTABLE: [persistAtom]
+});
+
+export const userEmailState = atom({
+    key: 'userEmailState',
+    default: '',
+    effects_UNSTABLE: [persistAtom]
+});
+
+export const userAddrState = atom({
+    key: 'userAddrState',
+    default: '',
+    effects_UNSTABLE: [persistAtom]
+});
+
+export const userPostCodeState = atom({
+    key: 'userPostCodeState',
+    default: '',
+    effects_UNSTABLE: [persistAtom]
+});
+
+
