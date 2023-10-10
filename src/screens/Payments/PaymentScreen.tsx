9e0279ace5469c05b3d0e6987066625c998c7786
sujinyang00 <sujinyang00@gmail.com>
2023-10-10 15:11:07 +0900

    결제 수정

--- src/screens/Payments/PaymentScreen.tsx
+++ src/screens/Payments/PaymentScreen.tsx
@@ -12,8 +12,8 @@ import {Currency} from "@portone/browser-sdk/dist/v2/entity/Currency";
 
 const impKey = process.env.REACT_APP_IMP_KEY;
 const storeId: string = process.env.REACT_APP_IMP_STORE_ID || '';
-const currency: Currency = Currency.KRW;
-const provider: PgProvider = PgProvider.NICE_V2;
+//const currency: Currency = Currency.KRW; //sdk 못찾음
+//const provider: PgProvider = PgProvider.NICE_V2; //sdk 못찾음
 const payMethod: PayMethod = PayMethod.PAY_METHOD;
 const reactapphomeurl= process.env.REACT_APP_PUBLIC_URL;
 
@@ -35,13 +35,53 @@ const PaymentScreen: React.FC = () => {
 
     useEffect(() => {
 
+        impPay();
+
+
         //todo --포트원 결제창 호출
-        requestPayment(storeId, orderId, goodsName, amount, currency, payMethod, returnUrl);
+        //requestPayment(storeId, orderId, goodsName, amount, currency, payMethod, returnUrl);
 
         //todo --나이스페이 결제창 호출
         //nicePay(clientId, method, orderId, amount, goodsName, returnUrl) //나이스페이 결제 요청
 
     }, []);
+
+    const impPay = () => {
+        if (!window.IMP) return;
+        /* 1. 가맹점 식별하기 */
+        const { IMP } = window;
+        IMP.init(storeId); // 가맹점 식별코드
+
+        /* 2. 결제 데이터 정의하기 */
+        const data: RequestPayParams = {
+            pg: "nice_v2.iamport00m", // PG사 : https://developers.portone.io/docs/ko/tip/pg-2 참고
+            pay_method: "card", // 결제수단
+            merchant_uid: orderId, // 주문번호
+            amount: amount, // 결제금액
+            name: goodsName, // 주문명
+
+            buyer_name: "홍길동", // 구매자 이름
+            buyer_tel: "01012341234", // 구매자 전화번호
+            buyer_email: "example@example.com", // 구매자 이메일
+            buyer_addr: "신사동 661-16", // 구매자 주소
+            buyer_postcode: "06018", // 구매자 우편번호
+        };
+
+        /* 4. 결제 창 호출하기 */
+        IMP.request_pay(data, callback);
+    }
+    /* 3. 콜백 함수 정의하기 */
+    function callback(response: RequestPayResponse) {
+        const { success, error_msg } = response;
+
+        if (success) {
+            alert("결제 성공");
+            window.location.href = returnUrl;
+        } else {
+            alert(`결제 실패: ${error_msg}`);
+        }
+    }
+
 
     function requestPayment(storeId: string, paymentId: string, orderName: string, amount: number, currency: Currency, method: PayMethod, returnUrl: string) {
         PortOne.requestPayment({
@@ -166,20 +206,20 @@ const PaymentScreen: React.FC = () => {
     );
 };
 
-/*export type RequestPayResponseCallback = (response: RequestPayResponse) => void
+export type RequestPayResponseCallback = (response: RequestPayResponse) => void
 export interface Iamport {
-    init: (accountID: string) => void
+    init: (accountID: string) => void;
     request_pay: (
         params: RequestPayParams,
-        callback?: RequestPayResponseCallback,
-    ) => void
+        callback?: RequestPayResponseCallback
+    ) => void;
 }
-//IMP 선언 시 에러 잠재우기 위해
+
 declare global {
     interface Window {
-        IMP?: Iamport
+        IMP?: Iamport;
     }
-}*/
+}
 
 //AUTHNICE 선언 시 에러 잠재우기 위해
 declare global {
