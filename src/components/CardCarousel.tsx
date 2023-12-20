73d9bc6dd3907aa54210eafcf0b6980052cae79f
sujinyang00 <sujinyang00@gmail.com>
2023-12-20 16:25:30 +0900

    Fix: CORS 에러 핸들러

--- src/components/CardCarousel.tsx
+++ src/components/CardCarousel.tsx
@@ -19,7 +19,6 @@ import "slick-carousel/slick/slick-theme.css";
 
 import axios from "axios";
 import {indigo} from "@mui/material/colors";
-import {axiosPrivateInstance} from "../services/AxiosApiService";
 
 const baseUrl = process.env.REACT_APP_BASE_URL
 
@@ -38,20 +37,32 @@ const CardCarousel = () => {
     const cardRegister = useState({"id": 0, "cardCode": "", "cardName":"","cardNo":"","cardAbleStatus":""});
     const [cardId, setCardId] = useRecoilState(cardIdState);
     const [payable, setPayAble] = useRecoilState(payAbleState);
-    const [token, setToken] = useRecoilValue(accessTokenState);
+    const token = useRecoilValue(accessTokenState);
 
     const fetchData = async () => {
         try {
-            axiosPrivateInstance.get(`/order/pay/card`, {})
-                .then(function (response) {
-                    console.log(`# CardCarousel --카드 데이터 response : ${JSON.stringify(response.data.result, null, 2)}`);
+            const config = {
+                headers: {
+                    //todo token으로 바꾸기
+                    "X-AUTH-TOKEN": token,
+                    "Header": token,
+                    "Access-Control-Allow-Headers": token,
+                    "Access-Control-Allow-Origin": `https://www.official-match.kr`,
+                    "Access-Control-Allow-Credentials": true,
+                }
+            };
+            axios.get(baseUrl + `/order/pay/card`, config)
+                .then((response) => {
                     console.log('# CardCarousel -- axios get detail 요청 성공');
-
                     setItems(response.data.result);
-                    console.log(`# CardCarousel --카드 데이터 items : ${JSON.stringify(items, null, 2)}`);
-
                     setPData([...response.data.result, ...pdata]);
+
+                    console.log(`# CardCarousel --카드 데이터 response : ${JSON.stringify(response.data.result, null, 2)}`);
+                    console.log(`# CardCarousel --카드 데이터 items : ${JSON.stringify(items, null, 2)}`);
                     console.log(`# CardCarousel --카드 데이터 pdata: ${JSON.stringify(pdata, null, 2)}`);
+
+                    // console.log('pdataaaaa : '+pdata.contents);
+                    // console.log('pdata:', JSON.stringify(pdata, null, 2));
                 })
                 .catch((error) => {
                     console.error('# CardCarousel Error fetching data:', error);
